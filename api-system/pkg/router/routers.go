a1363c6e994fc639364aa58deec812dbf0a79ede
yuxiang_wu <axiangcoding@gmail.com>
2022-01-09 23:05:11 +0800

    修复base path配置不生效

--- api-system/pkg/router/routers.go
+++ api-system/pkg/router/routers.go
@@ -23,9 +23,9 @@ func InitRouter() *gin.Engine {
 }
 
 func setRouterV1(r *gin.Engine) {
-	groupV1 := r.Group("/api/v1")
+	groupV1 := r.Group(conf.Config.Server.BasePath + "/v1")
 	{
-		demo := groupV1.Group("/demo", middleware.AuthCheck())
+		demo := groupV1.Group("/demo")
 		{
 			demo.GET("/get", v1.DemoGet)
 			demo.POST("/post", v1.DemoPost)
