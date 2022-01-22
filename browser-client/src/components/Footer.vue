e2e81ced40f105b7adde7a9735dcbb5935f2c0ff
yuxiang_wu <axiangcoding@gmail.com>
2022-01-22 21:58:37 +0800

    新增脚部栏内容

--- browser-client/src/components/Footer.vue
+++ browser-client/src/components/Footer.vue
@@ -1,22 +1,27 @@
 <template>
-	<n-layout-footer></n-layout-footer>
+  <n-layout-footer>
+    <n-space vertical>
+      <n-text strong>安东星 v0.2.0</n-text>
+      <div>
+        <n-button quaternary @click="router.push({name:'about'})">关于我们</n-button>
+        <n-divider vertical/>
+        <n-button quaternary>问题反馈</n-button>
+      </div>
+      <n-text italic>安东星开发小组 All Rights Reserved.</n-text>
+    </n-space>
+
+  </n-layout-footer>
 </template>
 
-<script lang="ts">
-  import { NLayoutFooter } from 'naive-ui';
-  import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import {useRouter} from "vue-router";
 
-  export default defineComponent({
-	components: {
-		NLayoutFooter,
-	},
-})
+const router = useRouter()
 </script>
 
-<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 .n-layout-footer {
-		height: $footerHeight;
-	}
+  height: $footerHeight;
+}
 </style>
