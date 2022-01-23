79bf7d877905ee444d1939b9c6736db7c0748fb0
yuxiang_wu <axiangcoding@gmail.com>
2022-01-23 14:12:07 +0800

    新增底部栏商业合作

--- browser-client/src/components/Footer.vue
+++ browser-client/src/components/Footer.vue
@@ -6,17 +6,34 @@
         <n-button quaternary @click="router.push({name:'about'})">关于我们</n-button>
         <n-divider vertical/>
         <n-button quaternary>问题反馈</n-button>
+        <n-divider vertical/>
+        <n-button quaternary @click="showModal=true">商业合作</n-button>
       </div>
       <n-text italic>安东星 All Rights Reserved.</n-text>
     </n-space>
-
+    <n-modal v-model:show="showModal">
+      <n-card
+          style="width: 600px;"
+          title="商业合作"
+          :bordered="false"
+          size="huge"
+          role="dialog"
+          aria-modal="true"
+      >
+        <template #header-extra></template>
+        <div>邮箱：<a href="mailto:axiangcoding@gmail.com" target="_blank">axiangcoding@gmail.com</a></div>
+        <!--<template #footer> 人也是要恰饭的嘛</template>-->
+      </n-card>
+    </n-modal>
   </n-layout-footer>
 </template>
 
 <script lang="ts" setup>
 import {useRouter} from "vue-router";
+import {ref} from "vue";
 
 const router = useRouter()
+const showModal = ref(false)
 </script>
 
 
