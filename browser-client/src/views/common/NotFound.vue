5e849d22f44ab6acf0c3d04df53696ae57800055
yuxiang_wu <axiangcoding@gmail.com>
2022-01-23 13:47:14 +0800

    404界面完善

--- browser-client/src/views/common/NotFound.vue
+++ browser-client/src/views/common/NotFound.vue
@@ -1,13 +1,20 @@
 <template>
-  <n-result status="404" title="404 资源不存在" description="生活总归带点荒谬">
-    <template #footer>
-      <n-button>找点乐子吧</n-button>
-    </template>
-  </n-result>
+  <n-card :bordered="false">
+    <n-result status="404" title="404 资源不存在" description="生活总归带点荒谬">
+      <template #footer>
+        <n-button type="primary" @click="router.back()">返回上一页</n-button>
+      </template>
+    </n-result>
+  </n-card>
+
 </template>
 
 <script lang="ts" setup>
-import {NResult} from "naive-ui";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+
 </script>
 
 <style scoped>
