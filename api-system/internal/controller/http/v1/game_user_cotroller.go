0ea5a252dc07db3316d3c6007860f9eeacfe4b81
axiangcoding <axiangcoding@gmail.com>
2023-02-21 21:23:29 +0800

    修改v1/wt/profile接口返回值

--- api-system/internal/controller/http/v1/game_user_cotroller.go
+++ api-system/internal/controller/http/v1/game_user_cotroller.go
@@ -1,11 +1,19 @@
 package v1
 
 import (
+	"errors"
+	"github.com/axiangcoding/antonstar-bot/internal/data/display"
 	"github.com/axiangcoding/antonstar-bot/internal/entity/app"
 	"github.com/axiangcoding/antonstar-bot/internal/entity/e"
 	"github.com/axiangcoding/antonstar-bot/internal/service"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
+
+type ProfileResp struct {
+	Found   bool              `json:"found"`
+	Profile *display.GameUser `json:"profile,omitempty"`
+}
 
 // GameUserProfile
 // @Summary  获取游戏内玩家数据
@@ -17,9 +25,18 @@ func GameUserProfile(c *gin.Context) {
 	nick := c.Query("nick")
 	profile, err := service.FindGameProfile(nick)
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			app.Success(c, ProfileResp{
+				Found: false,
+			})
+			return
+		}
 		app.BizFailed(c, e.Error, err)
 		return
 	}
 	displayGameUser := profile.ToDisplayGameUser()
-	app.Success(c, displayGameUser)
+	app.Success(c, ProfileResp{
+		Found:   true,
+		Profile: &displayGameUser,
+	})
 }
